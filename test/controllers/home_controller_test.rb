cc27997deffe5c187af7ad35892aa4775c8c144d
MikelLiza <mikellizarralde4@gmail.com>
2020-12-04 03:11:42 -0800

    Added testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,11 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get home_home_url
     assert_response :success
+
+    assert_select 'title', 'Lizard English Academy'
+    assert_select '.title', 'Lizard English Academy'
+    assert_select '.welcome', 'Learn, Live & Discover the English language with Lizard English Academy'
+    assert_select '.welcoming_message', 'With small class sizes, wonderful teachers and great support staff, come study at Lizard English Academy'
   end
 
 end
