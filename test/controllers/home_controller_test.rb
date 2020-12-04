fba7b16d8076e3c2aaca0748ec28bdebf60861ef
MikelLiza <mikellizarralde4@gmail.com>
2020-12-04 14:44:50 -0800

    Added contact form

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -11,4 +11,26 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_select '.welcoming_message', 'With small class sizes, wonderful teachers and great support staff, come study at Lizard English Academy'
   end
 
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+    assert_template layout: 'application'
+    assert_select 'title', 'Lizard English Academy'
+    assert_select 'h1', 'Contact Us'
+    assert_select 'p', 'Complete the following form to get in touch with us.'
+  end
+
+  test "should post request contact but no email" do
+    post request_contact_url
+    assert_response :redirect
+    assert_not_empty flash[:alert]
+    assert_nil flash[:notice]
+  end
+
+  test "should post request contact" do
+    post request_contact_url, params: {name: "Tester", email: "test@me.com", telephone: "1234567890", message: "Yo"}
+    assert_response :redirect
+    assert_nil flash[:alert]
+    assert_not_empty flash[:notice]
+  end
 end
