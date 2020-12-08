a8fd41cd821beba1aff198e76d26a371cfb44a90
MikelLiza <mikellizarralde4@gmail.com>
2020-12-08 10:52:28 -0800

    Added JQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,57 @@
 // about supported directives.
 //
 //= require bootstrap-sprockets
-//= require rails-ujs
+//= require jquery3
+//= require jquery_ujs
+//= require jquery.validate
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+function ValidateContactForm()
+{
+  $("#contact_id").validate({
+    errorClass: "error fail-alert",
+    validClass: "valid success-alert",
+    rules: {
+      name: {
+        required: true,
+        minlength: 3
+      },
+      email: {
+        required: true,
+        email: true,
+        maxlength: 255,
+      },
+      telephone: {
+        required: true,
+        number: true,
+        maxlength: 12,
+        min: 0,
+      },
+      message: 'required',
+    },
+    messages : {
+      name: {
+        minlength: "Name should be at least 3 characters"
+      },
+      email: {
+        required: "Please enter your email",
+        email: "The email should be in the format: abc@email.tld",
+        maxlength: "The email should be shorter"
+      },
+      telephone: {
+        required: "Please enter your telephone number",
+        number: "Please enter a number",
+        maxlength: "Number must be 12 numbers long at most",
+        min: "Do not enter negative numbers"
+      }
+    }
+  });
+}
+$(document).ready(function(){
+  if(document.getElementById('contact_id'))
+  {
+    ValidateContactForm();
+  }
+});
